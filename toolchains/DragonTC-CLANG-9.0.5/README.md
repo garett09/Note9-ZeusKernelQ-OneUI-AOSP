c2f349e1d6f1a51ac5c242b01603ece5f6bbae3f
synt4x93 <synt4x.93@gmail.com>
2020-06-12 02:46:21 +0100

    toolchain: nuke

--- toolchains/DragonTC-CLANG-9.0.5/README.md
+++
@@ -1,3 +0,0 @@
-# DragonTC-CLANG-9.0
-
-Custom Pre-Built Dragon TC Clang 9.0 for linux86_64 architecture, linked to my environment.
--- toolchains/DragonTC-CLANG-9.0.5/include/llvm/Support/VCSRevision.h
+++
@@ -1,2 +0,0 @@
-#define LLVM_REVISION "7bcfd53882a392fc715c14b35ea77aad131f9778"
-#define LLVM_REPOSITORY "/home/hana/Mido-MSM8953-Test-CLANG/dtc/llvm"
